c8bc5dc15e4a012ecdd386783adbe4a5626e1647
Simon Maresz <simon.maresz@sky.uk>
2025-02-19 15:37:41 +0100

    Added axios

--- frontend/src/api/fetchers.ts
+++ frontend/src/api/fetchers.ts
@@ -1,22 +1,16 @@
+import axios from 'axios';
 import { print } from 'graphql';
 import { DocumentNode } from 'graphql';
 
 export const fetchGraphQL = async <T>(query: DocumentNode, variables?: Record<string, any>): Promise<T> => {
-  const res = await fetch('/graphql', {
-    method: 'POST',
+  const { data } = await axios.post('/graphql', {
+    query: print(query),
+    variables,
+  }, {
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({
-      query: print(query),
-      variables,
-    }),
   });
 
-  if (!res.ok) {
-    throw new Error('Network response was not ok');
-  }
-
-  const { data } = await res.json();
-  return data;
+  return data.data;
 }; 
