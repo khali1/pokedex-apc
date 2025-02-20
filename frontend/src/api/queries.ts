a0ec9ca6d61936d560ea78afae3f4630ff03ed2f
Simon Maresz <simon.maresz@sky.uk>
2025-02-20 11:19:22 +0100

    [LIST] Added basic search

--- frontend/src/api/queries.ts
+++ frontend/src/api/queries.ts
@@ -1,10 +1,10 @@
 import { gql } from 'graphql-tag';
-
 export const GET_POKEMONS = gql`
-  query GetPokemons {
-    pokemons(query: { limit: 10, offset: 0 }) {
+  query GetPokemons($search: String = null) {
+    pokemons(query: { limit: 10, offset: 0, search: $search }) {
       edges {
         name
+        types
       }
     }
   }
--- frontend/src/app/browse/page.tsx
+++ frontend/src/app/browse/page.tsx
@@ -5,18 +5,21 @@ import { GQLQuery } from '../../../graphqlTypes';
 import { GET_POKEMONS } from '@/api/queries';
 import { fetchGraphQL } from '@/api/fetchers';
 import { PokemonCard } from '@/components/PokemonCard/PokemonCard';
+import { useState } from 'react';
 
 export default function BrowsePage() {
+  const [search, setSearch] = useState('');
   const { data, error, isLoading } = useQuery<GQLQuery>({
-    queryKey: ['pokemons'],
-    queryFn: () => fetchGraphQL(GET_POKEMONS),
+    queryKey: ['pokemons', search],
+    queryFn: () => fetchGraphQL(GET_POKEMONS, { search }),
   });
 
-  if (isLoading) return <div>Loading...</div>;
+  // if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
 
   return (
     <div className={styles.container}>
+        <div><input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)}/></div>
         {data?.pokemons.edges.map((pokemon) => (
           <PokemonCard key={pokemon.name} pokemon={pokemon} />
         ))}
