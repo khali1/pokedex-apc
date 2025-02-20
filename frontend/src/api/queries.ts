6557c644a417352777e705ed930eb3ad1214518f
Simon Maresz <simon.maresz@sky.uk>
2025-02-20 11:47:18 +0100

    [LIST] Added basic filter

--- frontend/src/api/queries.ts
+++ frontend/src/api/queries.ts
@@ -1,11 +1,17 @@
 import { gql } from 'graphql-tag';
 export const GET_POKEMONS = gql`
-  query GetPokemons($search: String = null) {
-    pokemons(query: { limit: 10, offset: 0, search: $search }) {
+  query GetPokemons($search: String = null, $filter: PokemonFilterInput = null) {
+    pokemons(query: { limit: 10, offset: 0, search: $search, filter: $filter }) {
       edges {
         name
         types
       }
     }
+  }
+`;
+
+export const GET_POKEMON_TYPES = gql`
+  query GetPokemonTypes {
+    pokemonTypes
   }
 `;
--- frontend/src/app/browse/page.tsx
+++ frontend/src/app/browse/page.tsx
@@ -1,17 +1,29 @@
-'use client';
-import styles from './page.module.scss';
-import { useQuery } from '@tanstack/react-query';
-import { GQLQuery } from '../../../graphqlTypes';
-import { GET_POKEMONS } from '@/api/queries';
-import { fetchGraphQL } from '@/api/fetchers';
-import { PokemonCard } from '@/components/PokemonCard/PokemonCard';
-import { useState } from 'react';
+"use client";
+import styles from "./page.module.scss";
+import { useQuery } from "@tanstack/react-query";
+import { GQLQuery } from "../../../graphqlTypes";
+import { GET_POKEMON_TYPES, GET_POKEMONS } from "@/api/queries";
+import { fetchGraphQL } from "@/api/fetchers";
+import { PokemonCard } from "@/components/PokemonCard/PokemonCard";
+import { useState } from "react";
+import Select from "react-select";
 
 export default function BrowsePage() {
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
+  const [type, setType] = useState<string | null>(null);
+
+  const { data: typesData } = useQuery<GQLQuery>({
+    queryKey: ["pokemonTypes"],
+    queryFn: () => fetchGraphQL(GET_POKEMON_TYPES),
+  });
+
   const { data, error, isLoading } = useQuery<GQLQuery>({
-    queryKey: ['pokemons', search],
-    queryFn: () => fetchGraphQL(GET_POKEMONS, { search }),
+    queryKey: ["pokemons", search, type],
+    queryFn: () =>
+      fetchGraphQL(GET_POKEMONS, {
+        search,
+        filter: { type },
+      }),
   });
 
   // if (isLoading) return <div>Loading...</div>;
@@ -19,10 +31,27 @@ export default function BrowsePage() {
 
   return (
     <div className={styles.container}>
-        <div><input type="text" placeholder="Search" value={search} onChange={(e) => setSearch(e.target.value)}/></div>
-        {data?.pokemons.edges.map((pokemon) => (
-          <PokemonCard key={pokemon.name} pokemon={pokemon} />
-        ))}
+      <div>
+        <input
+          type="text"
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <Select
+          isClearable
+          options={
+            typesData?.pokemonTypes.map((type) => ({
+              label: type,
+              value: type.toLowerCase(),
+            })) || []
+          }
+          onChange={(e) => setType(e?.value || null)}
+        />
+      </div>
+      {data?.pokemons.edges.map((pokemon) => (
+        <PokemonCard key={pokemon.name} pokemon={pokemon} />
+      ))}
     </div>
   );
-} 
+}
