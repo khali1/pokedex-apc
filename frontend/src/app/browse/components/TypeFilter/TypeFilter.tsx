11c00a2d1301bee483b807c03bd8c8ed756f089f
Simon Maresz <simon.maresz@sky.uk>
2025-02-24 13:33:38 +0100

    [FIX] Replaced react select with mantine/multiselect

--- frontend/src/app/browse/components/TypeFilter/TypeFilter.tsx
+++ frontend/src/app/browse/components/TypeFilter/TypeFilter.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { useMemo } from "react";
-import Select from "react-select";
+import { MultiSelect } from "@mantine/core";
 
 const TypeFilter = ({
   value,
@@ -11,28 +10,8 @@ const TypeFilter = ({
   options: string[];
   onChange: (value: string[] | null) => void;
 }) => {
-  const memoOptions = useMemo(() => {
-    return options?.map((type) => ({
-      label: type,
-      value: type,
-    }));
-  }, [options]);
-
   return (
-    <Select
-      isClearable
-      isMulti
-      value={
-        value?.map((type) => ({
-          label: type,
-          value: type,
-        })) || []
-      }
-      options={memoOptions}
-      onChange={(e) => {
-        onChange(e && e.length ? e.map((value) => value.value) : null);
-      }}
-    />
+    <MultiSelect clearable value={value} data={options} onChange={onChange} />
   );
 };
 
