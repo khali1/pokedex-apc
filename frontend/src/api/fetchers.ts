b657079aaf69e077021a1c96b6e600c17087f0de
Simon Maresz <simon.maresz@sky.uk>
2025-02-27 11:30:33 +0100

    [FIX] 'fixed' types for fetcher

--- frontend/src/api/fetchers.ts
+++ frontend/src/api/fetchers.ts
@@ -1,16 +1,23 @@
-import axios from 'axios';
-import { print } from 'graphql';
-import { DocumentNode } from 'graphql';
+import axios from "axios";
+import { print } from "graphql";
+import { DocumentNode } from "graphql";
 
-export const fetchGraphQL = async <T>(query: DocumentNode, variables?: Record<string, any>): Promise<T> => {
-  const { data } = await axios.post('/graphql', {
-    query: print(query),
-    variables,
-  }, {
-    headers: {
-      'Content-Type': 'application/json',
+export const fetchGraphQL = async <T>(
+  query: DocumentNode,
+  variables?: Record<string, unknown>
+): Promise<T> => {
+  const { data } = await axios.post(
+    "/graphql",
+    {
+      query: print(query),
+      variables,
     },
-  });
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
   return data.data;
-}; 
+};
